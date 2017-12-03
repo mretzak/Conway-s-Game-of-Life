b58ea6f617b169c1f641794d34b0c7d9ab59e4fe
Mike Retzak <mike.retzak@mobiwm.com>
2017-12-03 16:24:41 -0500

    Add autoplay, styles

--- assets/javascripts/controllers/world.controller.js
+++ assets/javascripts/controllers/world.controller.js
@@ -3,20 +3,25 @@
 
   angular.module('app').controller('WorldController', WorldController);
 
-  WorldController.$inject = ['golFactory'];
+  WorldController.$inject = ['$interval', 'golFactory'];
 
-  function WorldController(golFactory) {
+  function WorldController($interval, golFactory) {
     var vm = this;
-    var defaultGridSize = 10;
     var createWorldRequest = golFactory.createWorld();
     var nextGenRequest = golFactory.getNextGen();
 
-    vm.gridSize = defaultGridSize;
     vm.world = [];
+    vm.gridSize = 20;
+    vm.autoPlayInterval = null;
+    vm.autoPlay = false;
+    vm.worldCssClass = 'grid-20';
 
     vm.createWorld = createWorld;
     vm.toggleCell = toggleCell;
+    vm.clearWorld = clearWorld;
     vm.getNextGen = getNextGen;
+    vm.toggleAutoPlay = toggleAutoPlay;
+
     vm.$onInit = onInit;
     vm.$onDestroy = onDestroy;
 
@@ -30,11 +35,25 @@
     }
 
     function createWorld() {
+      cancelAutoPlay();
+      vm.worldCssClass = setWorldCssClass();
       createWorldRequest.perform(vm.gridSize).then(setWorld);
     }
 
     function setWorld(response) {
+      // Cancel the interval and turn off autoplay if the world is stabilized
+      if (angular.equals(vm.world, response)) {
+        cancelAutoPlay();
+        return false;
+      }
+
       vm.world = response;
+    }
+
+    function clearWorld() {
+      vm.world.map(function(row) {
+        row.fill(0);
+      });
     }
 
     function toggleCell(row, cell) {
@@ -44,6 +63,40 @@
 
     function getNextGen() {
       nextGenRequest.perform(vm.world).then(setWorld);
+    }
+
+    function toggleAutoPlay() {
+      if (vm.autoPlay) {
+        vm.autoPlayInterval = $interval(function () {
+          getNextGen();
+        }, 500);
+      } else {
+        $interval.cancel(vm.autoPlayInterval);
+      }
+    }
+
+    function cancelAutoPlay() {
+      if (vm.autoPlay) {
+        vm.autoPlay = false;
+        $interval.cancel(vm.autoPlayInterval);
+      }
+    }
+
+    function setWorldCssClass() {
+      switch (true) {
+        case vm.gridSize >= 50:
+          return 'grid-50';
+        case vm.gridSize >= 40:
+          return 'grid-40';
+        case vm.gridSize >= 30:
+          return 'grid-30';
+        case vm.gridSize >= 20:
+          return 'grid-20';
+        case vm.gridSize >= 10:
+          return 'grid-10';
+        default:
+          return 'grid-20';
+      }
     }
   }
 })();
